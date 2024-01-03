46518c4ef0124c122b15e63f6b515b34e0a76131
rateeBeest <rsharma10@sfsu.edu>
2024-01-03 11:53:10 -0800

    Init Commit with file added

---
+++ rangelist.py
@@ -0,0 +1,15 @@
+#to find the squares of the number using range and list
+
+mySquareList = []
+
+for myValue in range(1,11):
+    mySquare = myValue ** 2
+    mySquareList.append(mySquare)
+
+
+print(mySquareList)
+
+#prints [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+#range from 1 to 10
+
